d00718eabf6cd1f7d6c7f5851519b1508956ad70
Mike Hepworth <mhepworth@highbridgeconsultants.com>
2020-11-26 11:24:16 -0700

    test

--- Preference Form AJAX Controller.js
+++ Preference Form AJAX Controller.js
@@ -29,19 +29,19 @@
 	  "Mkto_Research_News__c","false"
 	  )
 	ENDIF
-	 Set @rs= RetrieveSalesforceObjects('Contact', @contactFields, 'Id', '=', @contactKey);
-	  IF RowCount(@rs) > 0 THEN
-	    FOR @publicationListIndex = 1 TO RowCount(@publicationLists) DO
-	      Set @publicationListTitle = Field(Row(@publicationLists,@publicationListIndex),"Title")
-	      Set @publicationListCRMFieldName = Field(Row(@publicationLists,@publicationListIndex),"CRM_Field_Name")
-	      Set @publicationListValue = Field(Row(@rs,1),@publicationListCRMFieldName)
-	      //Add object to array
-	      Set @settingsObject = Concat(@settingsObject,"{""Title"":""",@publicationListTitle,""",""CRMFieldName"":""",@publicationListCRMFieldName,""",""Value"":""",@publicationListValue,"""}",Iif(@publicationListIndex == RowCount(@publicationLists),"",","))
-	    NEXT @publicationListIndex
+	Set @rs= RetrieveSalesforceObjects('Contact', @contactFields, 'Id', '=', @contactKey);
+	IF RowCount(@rs) > 0 THEN
+	  FOR @publicationListIndex = 1 TO RowCount(@publicationLists) DO
+	    Set @publicationListTitle = Field(Row(@publicationLists,@publicationListIndex),"Title")
+	    Set @publicationListCRMFieldName = Field(Row(@publicationLists,@publicationListIndex),"CRM_Field_Name")
+	    Set @publicationListValue = Field(Row(@rs,1),@publicationListCRMFieldName)
+	    //Add object to array
+	    Set @settingsObject = Concat(@settingsObject,"{""Title"":""",@publicationListTitle,""",""CRMFieldName"":""",@publicationListCRMFieldName,""",""Value"":""",@publicationListValue,"""}",Iif(@publicationListIndex == RowCount(@publicationLists),"",","))
+		NEXT @publicationListIndex
 
-	    Set @firstName = Field(Row(@rs,1),'FirstName');
-	    Set @lastName = Field(Row(@rs,1),'LastName');
-		ENDIF
+	  Set @firstName = Field(Row(@rs,1),'FirstName');
+	  Set @lastName = Field(Row(@rs,1),'LastName');
+	ENDIF
 	Set @settingsObject = Concat(@settingsObject,"]");
 ]%%
 <script runat=server>
