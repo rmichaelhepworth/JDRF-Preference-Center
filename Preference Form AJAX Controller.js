cc75fd9fa5e0d9d09831a1ddc0c2918f4bb784db
Mike Hepworth <mhepworth@highbridgeconsultants.com>
2020-11-26 10:44:28 -0700

    test

--- Preference Form AJAX Controller.js
+++ Preference Form AJAX Controller.js
@@ -20,21 +20,31 @@
 		Variable.SetValue("@contactKey",Platform.Request.GetQueryStringParameter('contactKey'));
 </script>
 %%[
-	Set @rs= RetrieveSalesforceObjects('Contact', @contactFields, 'Id', '=', @contactKey);
-  Set @settingsObject = "[";
-  IF RowCount(@rs) > 0 THEN
-    FOR @publicationListIndex = 1 TO RowCount(@publicationLists) DO
-      Set @publicationListTitle = Field(Row(@publicationLists,@publicationListIndex),"Title")
-      Set @publicationListCRMFieldName = Field(Row(@publicationLists,@publicationListIndex),"CRM_Field_Name")
-      Set @publicationListValue = Field(Row(@rs,1),@publicationListCRMFieldName)
-      //Add object to array
-      Set @settingsObject = Concat(@settingsObject,"{""Title"":""",@publicationListTitle,""",""CRMFieldName"":""",@publicationListCRMFieldName,""",""Value"":""",@publicationListValue,"""}",Iif(@publicationListIndex == RowCount(@publicationLists),"",","))
-    NEXT @publicationListIndex
+	Set @settingsObject = "[";
+	IF (@action == 'update') THEN
+	  var @updateRecord
+	  set @updateRecord = UpdateSingleSalesforceObject(
+	  "Contact", @contactKey,
+	  "FirstName", @firstname,
+	  "LastName", @lastName,
+	  "Email", @email
+	  )
+	ELSE
+	 Set @rs= RetrieveSalesforceObjects('Contact', @contactFields, 'Id', '=', @contactKey);
+	  IF RowCount(@rs) > 0 THEN
+	    FOR @publicationListIndex = 1 TO RowCount(@publicationLists) DO
+	      Set @publicationListTitle = Field(Row(@publicationLists,@publicationListIndex),"Title")
+	      Set @publicationListCRMFieldName = Field(Row(@publicationLists,@publicationListIndex),"CRM_Field_Name")
+	      Set @publicationListValue = Field(Row(@rs,1),@publicationListCRMFieldName)
+	      //Add object to array
+	      Set @settingsObject = Concat(@settingsObject,"{""Title"":""",@publicationListTitle,""",""CRMFieldName"":""",@publicationListCRMFieldName,""",""Value"":""",@publicationListValue,"""}",Iif(@publicationListIndex == RowCount(@publicationLists),"",","))
+	    NEXT @publicationListIndex
 
-		Set @firstName = Field(Row(@rs,1),'FirstName');
-		Set @lastName = Field(Row(@rs,1),'LastName');
-  ENDIF
-  Set @settingsObject = Concat(@settingsObject,"]");
+	    Set @firstName = Field(Row(@rs,1),'FirstName');
+	    Set @lastName = Field(Row(@rs,1),'LastName');
+	  ENDIF
+	ENDIF
+	Set @settingsObject = Concat(@settingsObject,"]");
 ]%%
 <script runat=server>
     responseObject.contactKey = Variable.GetValue("@contactKey");
